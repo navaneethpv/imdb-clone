ff380043b0479e55cbb0fc0fd0385cbb340262bf
Navaneeth PV <navaneethpv550@gmail.com>
2025-08-19 22:40:50 +0300

    feat: enhance Navbar component layout and styling

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,11 +1,22 @@
-import React from 'react'
+import React from "react";
 import { FcFilmReel } from "react-icons/fc";
 const Navbar = () => {
   return (
-    <div>
-        <FcFilmReel className='text-5xl m-5'/>
+    <div className="flex justify-between items-center bg-gray-800 text-white p-4">
+        <FcFilmReel className="text-5xl" />
+      <div className="flex gap-15 justify-between items-center text-white p-2 ml-20">
+        <a href="#">
+          <li className="list-none font-bold">Watchlist</li>
+        </a>
+        <a href="#">
+          <li className="list-none font-bold">Latest</li>
+        </a>
+        <a href="#">
+          <li className="list-none font-bold">About</li>
+        </a>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default Navbar
+export default Navbar;
