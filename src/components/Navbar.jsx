c38fd49b539fb528f230c16702539f9a16d4164c
Navaneeth PV <navaneethpv550@gmail.com>
2025-08-19 23:01:15 +0300

    feat: update Navbar component links and icon size

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -3,13 +3,13 @@ import { FcFilmReel } from "react-icons/fc";
 const Navbar = () => {
   return (
     <div className="flex justify-between items-center bg-gray-800 text-white p-4">
-        <FcFilmReel className="text-5xl" />
+        <FcFilmReel className="text-6xl" />
       <div className="flex gap-15 justify-between items-center text-white p-2 ml-20">
-        <a href="#">
-          <li className="list-none font-bold">Watchlist</li>
+        <a href="/">
+          <li className="list-none font-bold">Home</li>
         </a>
-        <a href="#">
-          <li className="list-none font-bold">Latest</li>
+        <a href="/watchlist">
+          <li className="list-none font-bold">Watchlist</li>
         </a>
         <a href="#">
           <li className="list-none font-bold">About</li>
