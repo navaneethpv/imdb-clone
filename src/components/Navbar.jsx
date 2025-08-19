ccb804a8716427e701c9d1d951d46bf6e76ceece
Navaneeth PV <navaneethpv550@gmail.com>
2025-08-19 22:29:42 +0300

    feat: initialize React project with Vite and Tailwind CSS
    - Added package.json with dependencies for React, Tailwind CSS, and Vite
    - Included Vite logo SVG in public directory
    - Created main application component (App.jsx) with a Navbar component
    - Implemented Navbar component with a film reel icon
    - Set up global CSS file to import Tailwind CSS styles
    - Configured Vite with React and Tailwind CSS plugins

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import { FcFilmReel } from "react-icons/fc";
+const Navbar = () => {
+  return (
+    <div>
+        <FcFilmReel className='text-5xl m-5'/>
+    </div>
+  )
+}
+
+export default Navbar
