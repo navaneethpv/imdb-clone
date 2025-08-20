ff25cf7af58bc66b23f55a479bd411857842985c
Navaneeth PV <navaneethpv550@gmail.com>
2025-08-20 23:20:07 +0300

    feat: replace anchor tags with Link components in Navbar for routing

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,19 +1,20 @@
 import React from "react";
 import { FcFilmReel } from "react-icons/fc";
+import { Link} from "react-router-dom";
 const Navbar = () => {
   return (
     <div className="flex justify-between items-center bg-gray-800 text-white p-4">
         <FcFilmReel className="text-6xl" />
       <div className="flex gap-15 justify-between items-center text-white p-2 ml-20">
-        <a href="/">
+        <Link to="/">
           <li className="list-none font-bold">Home</li>
-        </a>
-        <a href="/watchlist">
+        </Link>
+        <Link to="/watchlist">
           <li className="list-none font-bold">Watchlist</li>
-        </a>
-        <a href="/about">
+        </Link>
+        <Link to="/about">
           <li className="list-none font-bold">About</li>
-        </a>
+        </Link>
       </div>
     </div>
   );
