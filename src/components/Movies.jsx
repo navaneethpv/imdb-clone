7744d43a2ab90a684837cd62b6511a122a2d7ebe
Navaneeth PV <navaneethpv550@gmail.com>
2025-08-21 00:40:14 +0300

    feat: update Movies component to display trending movies with MovieCards

--- src/components/Movies.jsx
+++ src/components/Movies.jsx
@@ -1,11 +1,28 @@
-import React from 'react'
+import React from "react";
+import MovieCards from "./MovieCards";
 
 const Movies = () => {
   return (
-    <div>
-        <h1>This is movies component</h1>
-    </div>
-  )
-}
+    <>
+      <div className="p-10">
+        <h1 className="text-3xl font-semibold text-center mt-10">Trending Movies</h1>
+      </div>
+      <div className="flex flex-wrap justify-between m-3">
+        <MovieCards />
+        <MovieCards />
+        <MovieCards />
+        <MovieCards />
+        <MovieCards />
+        <MovieCards />
+        <MovieCards />
+        <MovieCards />
+        <MovieCards />
+        <MovieCards />
+        <MovieCards />
+        <MovieCards />
+      </div>
+    </>
+  );
+};
 
-export default Movies
+export default Movies;
