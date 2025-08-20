3edf4a7a2b15aaa9f789db22e798fc7758f3d684
Navaneeth PV <navaneethpv550@gmail.com>
2025-08-20 23:44:50 +0300

    `Refactor Banner component: update banner image container height, background color, and text size.`

--- src/components/Banner.jsx
+++ src/components/Banner.jsx
@@ -4,8 +4,8 @@ import bannerImage from '../assets/banner1.png'
 const Banner = () => {
   return (
     <div className='flex flex-col justify-end text-white h-[600px] bg-no-repeat bg-center bg-cover' style={{backgroundImage: `url(${bannerImage})`}}>
-        <div className='h-10 bg-blue-400 flex justify-center items-center mb-1'> 
-           <h1 className='font-bold text-2xl'>Avengers</h1>
+        <div className='h-7 bg-violet-800 flex justify-center items-center'> 
+           <h1 className='font-bold text-xl'>Avengers</h1>
         </div>
     </div>
   )
