4bcc1f24b93c3bbc2daa8349c989c6fd5e13ec21
Navaneeth PV <navaneethpv550@gmail.com>
2025-08-21 00:14:33 +0300

    fix: update banner image and title for consistency in the Banner component

--- src/components/Banner.jsx
+++ src/components/Banner.jsx
@@ -1,11 +1,11 @@
 import React from 'react'
-import bannerImage from '../assets/banner1.png'
+import bannerImage from '../assets/banner2.jpg'
 
 const Banner = () => {
   return (
     <div className='flex flex-col justify-end text-white h-[80vh] bg-no-repeat bg-center bg-cover' style={{backgroundImage: `url(${bannerImage})`}}>
         <div className='p-1 bg-violet-900/89 flex justify-center items-center'> 
-           <h1 className='font-bold text-xl'>Avengers</h1>
+           <h1 className='font-bold text-xl'>The Avengers</h1>
         </div>
     </div>
   )
