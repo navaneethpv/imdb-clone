4cfcc12928e848a17d5737210d7aef1c4d7f9724
Navaneeth PV <navaneethpv550@gmail.com>
2025-08-20 23:41:10 +0300

    feat: add Banner component to the home route for enhanced UI

--- src/App.jsx
+++ src/App.jsx
@@ -3,13 +3,14 @@ import WatchList from "./components/WatchList.jsx";
 import Movies from "./components/Movies.jsx";
 import About from "./components/About.jsx";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Banner from "./components/Banner.jsx";
 function App() {
   return (
     <>
       <BrowserRouter>
         <Navbar />
         <Routes>
-          <Route path="/" element={<Movies />} />
+          <Route path="/" element={<><Banner /> <Movies /></>} />
           <Route path="/watchlist" element={<WatchList />} />
           <Route path="/about" element={<About />} />
         </Routes>
