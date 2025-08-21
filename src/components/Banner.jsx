f73c1acae0b3694e47694e288f748b737de04504
Navaneeth PV <navaneethpv550@gmail.com>
2025-08-21 20:02:11 +0300

    fix: adjust Banner component height for better visual consistency

--- src/components/Banner.jsx
+++ src/components/Banner.jsx
@@ -3,7 +3,7 @@ import bannerImage from '../assets/banner2.jpg'
 
 const Banner = () => {
   return (
-    <div className='flex flex-col justify-end text-white h-[80vh] bg-no-repeat bg-center bg-cover' style={{backgroundImage: `url(${bannerImage})`}}>
+    <div className='flex flex-col justify-end text-white h-[70vh] bg-no-repeat bg-center bg-cover' style={{backgroundImage: `url(${bannerImage})`}}>
         <div className='p-1 bg-violet-900/89 flex justify-center items-center'> 
            <h1 className='font-bold text-xl'>The Avengers</h1>
         </div>
