464b353ce5f7c765eb8facdec6641a64a1b636bd
Navaneeth PV <navaneethpv550@gmail.com>
2025-08-21 08:51:40 +0300

    fix: improve Navbar layout by adjusting structure and spacing

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,10 +1,13 @@
 import React from "react";
 import { FcFilmReel } from "react-icons/fc";
-import { Link} from "react-router-dom";
+import { Link } from "react-router-dom";
 const Navbar = () => {
   return (
     <div className="flex justify-between items-center bg-gray-800 text-white p-4">
+      <div className="flex items-center">
         <FcFilmReel className="text-6xl" />
+        <span className="text-4xl font-bold ml-2 text-gray-300">Movies.in</span>
+      </div>
       <div className="flex gap-15 justify-between items-center text-white p-2 ml-20">
         <Link to="/">
           <li className="list-none font-bold">Home</li>
