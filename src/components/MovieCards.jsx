7f66f38415ce549209c0e61fd623d050623faa48
Navaneeth PV <navaneethpv550@gmail.com>
2025-08-21 21:29:28 +0300

    fix: update MovieCards component to use dynamic poster paths from API

--- src/components/MovieCards.jsx
+++ src/components/MovieCards.jsx
@@ -1,12 +1,12 @@
 import React from "react";
 
-const MovieCards = () => {
+const MovieCards = ({posterPath}) => {
   return (
     <>
       <div
         className="h-100 w-[270px] bg-contain bg-center rounded-xl mb-10 hover:cursor-pointer hover:scale-108 transition-all duration-160"
         style={{
-          backgroundImage: `url(https://images-cdn.ubuy.co.in/6834eb72b1018ac26509b11b-black-panther-marvel-movie-poster.jpg)`,
+          backgroundImage: `url(https://image.tmdb.org/t/p/original/${posterPath.poster_path})`,
         }}
       ></div>
     </>
--- src/components/Movies.jsx
+++ src/components/Movies.jsx
@@ -1,28 +1,39 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import axios from "axios";
 import MovieCards from "./MovieCards";
+import { CiDeliveryTruck } from "react-icons/ci";
 
 const Movies = () => {
+  const [movies, setMovies] = useState([]);
+
+  useEffect(() => {
+    axios
+      .get(`https://api.themoviedb.org/3/trending/all/day?api_key=9c5a110ef3fd618e3157fe9991bb75bb&language=en-US`)
+      .then((response) => {
+        console.log(response.data.results);
+        setMovies(response.data.results);
+      });
+  }, []);
+
   return (
     <>
       <div className="p-10">
         <h1 className="text-3xl font-semibold text-center mt-10">Trending Movies</h1>
       </div>
       <div className="flex flex-wrap justify-between m-3">
-        <MovieCards />
-        <MovieCards />
-        <MovieCards />
-        <MovieCards />
-        <MovieCards />
-        <MovieCards />
-        <MovieCards />
-        <MovieCards />
-        <MovieCards />
-        <MovieCards />
-        <MovieCards />
-        <MovieCards />
+         {movies.map((movieObj) =>{
+           return (
+            <MovieCards
+              key={movieObj.id}
+              posterPath={movieObj}
+            />
+           )
+         })}
+
       </div>
     </>
   );
 };
 
 export default Movies;
+//https://api.themoviedb.org/3/trending/all/day?api_key=9c5a110ef3fd618e3157fe9991bb75bb&language=en-US
