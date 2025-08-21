3b7ee63e35132c751e501918239c007df74c6320
Navaneeth PV <navaneethpv550@gmail.com>
2025-08-21 09:44:39 +0300

    fix: adjust hover scale effect in MovieCards component for improved interaction

--- src/components/MovieCards.jsx
+++ src/components/MovieCards.jsx
@@ -3,9 +3,12 @@ import React from "react";
 const MovieCards = () => {
   return (
     <>
-      <div className="h-100 w-[270px] bg-contain bg-center rounded-xl mb-10 hover:cursor-pointer hover:scale-110 transition-all duration-160" style={{backgroundImage : `url(https://images-cdn.ubuy.co.in/6834eb72b1018ac26509b11b-black-panther-marvel-movie-poster.jpg)`}}>
-
-      </div>
+      <div
+        className="h-100 w-[270px] bg-contain bg-center rounded-xl mb-10 hover:cursor-pointer hover:scale-108 transition-all duration-160"
+        style={{
+          backgroundImage: `url(https://images-cdn.ubuy.co.in/6834eb72b1018ac26509b11b-black-panther-marvel-movie-poster.jpg)`,
+        }}
+      ></div>
     </>
   );
 };
