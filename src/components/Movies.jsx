342873fe0019982b568d3b755504758957aff220
Navaneeth PV <navaneethpv550@gmail.com>
2025-08-23 10:49:07 +0300

    `Added error handling to Movies component`

--- src/components/Movies.jsx
+++ src/components/Movies.jsx
@@ -6,6 +6,7 @@ import Pagination from "./Pagination";
 const Movies = () => {
   const [movies, setMovies] = useState([]);
   const [pageNo, setPageNo] = useState(1);
+  const [error, setError] = useState(false);
 
   const handlePrevPage = () => {
     if (pageNo > 1) {
@@ -25,15 +26,19 @@ const Movies = () => {
       .then((response) => {
         console.log(response.data.results);
         setMovies(response.data.results);
-      });
+      })
+      .catch((error) => {
+        console.log(error);
+        setError(true);
+      }
+      )
   }, [pageNo]);
 
   return (
     <>
       <div className="p-14">
-        <h1 className="text-3xl font-semibold text-center mt-10">
-          Trending Movies
-        </h1>
+        { error ? <h1 className="text-3xl font-semibold text-center mt-10">Error fetching data</h1> : <h1 className="text-3xl font-semibold text-center mt-10">Trending Movies</h1>}
+          
       </div>
       <div className="flex flex-wrap m-3 justify-center md:grid md:grid-cols-5 md:m-3 md:ml-14">
         {movies.map((movieObj) => {
@@ -52,4 +57,3 @@ const Movies = () => {
 };
 
 export default Movies;
-//https://api.themoviedb.org/3/trending/all/day?api_key=9c5a110ef3fd618e3157fe9991bb75bb&language=en-US
