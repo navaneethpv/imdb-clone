646f8e05cf1124e6f6b6e66d4dfe4bf02591ab59
Navaneeth PV <navaneethpv550@gmail.com>
2025-08-25 00:00:03 +0300

    `Refactored WatchList component: updated input field styles, table border and rounded corners, and corrected table header cell borders.`

--- src/components/WatchList.jsx
+++ src/components/WatchList.jsx
@@ -4,17 +4,19 @@ const WatchList = () => {
   return (
     <>
         <div className="flex flex-col items-center mt-10">
-          <input type="text" className="border-2 border-gray-300 p-4 w-[30%] font-bold outline-none" placeholder="Search your watchlist..."/>
+          <input type="text" className="border-2 border-gray-300 p-4 rounded-lg w-[30%] font-bold outline-none" placeholder="Search your watchlist..."/>
         </div>
         <div>
-          <table className="w-[90%] m-auto mt-15 border-2 text-center border-gray-200"> 
+          <table className="w-[90%] m-auto mt-10 border-0 border-gray-300 text-center rounded-lg"> 
             <thead className="">
-              <tr className='bg-gray-200 '>
-                <th className=" w-[50%] p-4 rounded-tl-lg border-gray-200 ">Name</th>
-                <th className="border-2 border-gray-200 p-4">Rating</th>
-                <th className="border-2 border-gray-200 p-4">Popularity</th>
-                <th className="border-2 border-gray-200 p-4 rounded-tr-lg">Genre</th>
-              </tr>
+              <tr className='bg-gray-200'>
+                <th className="border-0 border-gray-200 w-[50%] p-4 rounded-tl-lg">Name</th>
+                <th className="border-0 border-gray-200 p-4">Rating</th>
+                <th className="border-0 border-gray-200 p-4">Popularity</th>
+                <th className="border-0 border-gray-200 p-4 rounded-tr-lg">Genre</th>
+              </tr> 
+
+              
             </thead>
             <tbody>
 
@@ -25,4 +27,4 @@ const WatchList = () => {
   )
 }
 
-export default WatchList
+export default WatchList  
