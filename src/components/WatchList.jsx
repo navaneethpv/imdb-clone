8f58582c7eadae1bd4576eaaad8ec7001c314aac
Navaneeth PV <navaneethpv550@gmail.com>
2025-08-24 20:42:59 +0300

    `Added search input and table structure to WatchList component`

--- src/components/WatchList.jsx
+++ src/components/WatchList.jsx
@@ -2,9 +2,26 @@ import React from 'react'
 
 const WatchList = () => {
   return (
-    <div>
-        <h1>This is watchlist component</h1>
-    </div>
+    <>
+        <div className="flex flex-col items-center mt-10">
+          <input type="text" className="border-2 border-gray-300 p-4 rounded-lg w-[30%] font-bold outline-none" placeholder="Search your watchlist..."/>
+        </div>
+        <div>
+          <table className="w-[90%] m-auto mt-15 border-2 border-gray-300 text-center"> 
+            <thead className="">
+              <tr className='bg-gray-200'>
+                <th className="border-2 border-gray-200 w-[50%] p-4 rounded-tl-lg">Name</th>
+                <th className="border-2 border-gray-200 p-4">Rating</th>
+                <th className="border-2 border-gray-200 p-4">Popularity</th>
+                <th className="border-2 border-gray-200 p-4 rounded-tr-lg">Genre</th>
+              </tr>
+            </thead>
+            <tbody>
+
+            </tbody>
+          </table>
+        </div>
+    </>
   )
 }
 
