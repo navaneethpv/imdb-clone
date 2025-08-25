428b9ab0e67bb29a64507dd4efd266b781a6316f
Navaneeth PV <navaneethpv550@gmail.com>
2025-08-25 23:50:22 +0300

    `Refactor WatchList component: update table styles and classes`

--- src/components/WatchList.jsx
+++ src/components/WatchList.jsx
@@ -7,13 +7,13 @@ const WatchList = () => {
           <input type="text" className="border-2 border-gray-300 p-4 rounded-lg w-[30%] font-bold outline-none" placeholder="Search your watchlist..."/>
         </div>
         <div>
-          <table className="w-[90%] m-auto mt-10 border-0 border-gray-300 text-center rounded-lg"> 
-            <thead className="">
+          <table className="w-full m-auto mt-10 border-0 border-gray-300 text-gray-800 text-center"> 
+            <thead className="border-b-3 border-gray-300">
               <tr className='bg-gray-200'>
-                <th className="border-0 border-gray-200 w-[50%] p-4 rounded-tl-lg">Name</th>
-                <th className="border-0 border-gray-200 p-4">Rating</th>
-                <th className="border-0 border-gray-200 p-4">Popularity</th>
-                <th className="border-0 border-gray-200 p-4 rounded-tr-lg">Genre</th>
+                <th className="border-0 border-gray-200 w-[50%] p-2 rounded-tl-lg">Name</th>
+                <th className="border-0 border-gray-200 p-2">Rating</th>
+                  <th className="border-0 border-gray-200 p-2">Popularity</th>
+                <th className="border-0 border-gray-200 p-2 rounded-tr-lg">Genre</th>
               </tr> 
 
               
