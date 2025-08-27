94b2c24aa7b9764d3042856e76f86ef5b996c337
Navaneeth PV <navaneethpv550@gmail.com>
2025-08-27 23:22:56 +0300

    images and datas are added for make design

--- src/components/WatchList.jsx
+++ src/components/WatchList.jsx
@@ -10,17 +10,43 @@ const WatchList = () => {
           placeholder="Search your watchlist..."
         />
       </div>
-      <div>
-        <table className="w-[97%] m-auto mt-10 border-0 border-gray-300 text-gray-800 text-center">
+      <div className="overflow-hidden border border-gray-300 rounded-lg m-10">
+        <table className="w-full m-auto border-0 border-gray-300 text-gray-800 text-center">
           <thead className="border-b-3 border-gray-300">
             <tr className="bg-gray-200">
-              <th className="border-0 border-gray-200 w-[50%] p-2">Name</th>
-              <th className="border-0 border-gray-200 p-2">Rating</th>
-              <th className="border-0 border-gray-200 p-2">Popularity</th>
+              <th className="border-0 border-gray-200 w-[50%] p-3">Name</th>
+              <th className="border-0 border-gray-200 p-3">Rating</th>
+              <th className="border-0 border-gray-200 p-3">Popularity</th>
               <th className="border-0 border-gray-200 ">Genre</th>
             </tr>
           </thead>
-          <tbody></tbody>
+          <tbody>
+            <tr className="border-b-2 border-gray-300 hover:bg-gray-100">
+              <td className="border-0 border-gray-200 p-3 flex items-center">
+                <img
+                  src={`https://img.freepik.com/premium-psd/movie-poster_1009850-314.jpg?w=2000`}
+                  alt="Movie"
+                  className="w-[8rem] h-[4rem] object-cover rounded-sm"
+                />
+                <div className="mx-11">Movie bot</div>
+              </td>
+              <td className="border-0 border-gray-200 p-3">8.5</td>
+              <td className="border-0 border-gray-200 p-3">High</td>
+              <td className="border-0 border-gray-200 p-3">Action</td>
+            </tr>
+            <tr className="border-b-2 border-gray-300 hover:bg-gray-100">
+              <td className="border-0 border-gray-200 p-3">
+                <img
+                  src={`https://img.freepik.com/premium-psd/movie-poster_1009850-314.jpg?w=2000`}
+                  alt="Movie"
+                  className="w-[8rem] h-[4rem] object-cover rounded-sm"
+                />
+              </td>
+              <td className="border-0 border-gray-200 p-3">8.5</td>
+              <td className="border-0 border-gray-200 p-3">High</td>
+              <td className="border-0 border-gray-200 p-3">Action</td>
+            </tr>
+          </tbody>
         </table>
       </div>
     </>
